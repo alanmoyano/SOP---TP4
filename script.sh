b8fda27287263b72400ca92e621af259e87f30e9
Gonza <zavalagonzalo172@gmail.com>
2023-11-01 08:35:14 -0300

    Anda

--- script.sh
+++ script.sh
@@ -1,1 +1,18 @@
-echo "hola gonza"
+sudo groupadd asistente
+sudo groupadd operadores
+
+sudo useradd fer
+sudo useradd mar
+sudo useradd lopez
+
+sudo usermod fer   -G operadores 
+sudo usermod mar   -G operadores 
+sudo usermod lopez -G operadores 
+
+
+if [ "$1" != "" ]; then
+    nombre=pc$1
+
+    useradd $nombre
+    usermod $nombre -G asistente
+fi
