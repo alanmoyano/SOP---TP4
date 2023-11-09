83e3e7cc7fa59ba15598c7e8e345f314752c8a3e
Alan (linux) <alanmoyano203@gmail.com>
2023-11-09 14:39:46 -0300

    Agregados sudos

--- script.sh
+++ script.sh
@@ -1,18 +1,17 @@
-sudo groupadd asistente  2> /dev/null
-sudo groupadd operadores 2> /dev/null
+sudo groupadd asistente 2>/dev/null
+sudo groupadd operadores 2>/dev/null
 
-sudo useradd fer   2> /dev/null
-sudo useradd mar   2> /dev/null
-sudo useradd lopez 2> /dev/null
+sudo useradd fer 2>/dev/null
+sudo useradd mar 2>/dev/null
+sudo useradd lopez 2>/dev/null
 
-sudo usermod fer   -G operadores 2> /dev/null
-sudo usermod mar   -G operadores 2> /dev/null
-sudo usermod lopez -G operadores 2> /dev/null
-
+sudo usermod fer -G operadores 2>/dev/null
+sudo usermod mar -G operadores 2>/dev/null
+sudo usermod lopez -G operadores 2>/dev/null
 
 if [ "$1" != "" ]; then
     nombre=pc$1
 
-    useradd $nombre 2> /dev/null
-    usermod $nombre -G asistente 2> /dev/null
+    sudo useradd $nombre 2>/dev/null
+    sudo usermod $nombre -G asistente 2>/dev/null
 fi
